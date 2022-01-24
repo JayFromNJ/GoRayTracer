849f9acc27300193859f9fda05a860024196d380
Jason Wood <jayfromnj@gmail.com>
2022-01-24 15:25:05 -0500

    inside added to computations

--- object/computations.go
+++ object/computations.go
@@ -11,6 +11,7 @@ type Computations struct {
 	point   vector.Vector
 	eyev    vector.Vector
 	normalv vector.Vector
+	inside  bool
 }
 
 func (c *Computations) GetTime() float64                  { return c.t }
@@ -18,6 +19,7 @@ func (c *Computations) GetHittableObject() HittableObject { return c.hitObj }
 func (c *Computations) GetPoint() vector.Vector           { return c.point }
 func (c *Computations) GetEyeV() vector.Vector            { return c.eyev }
 func (c *Computations) GetNormalV() vector.Vector         { return c.normalv }
+func (c *Computations) IsInside() bool                    { return c.inside }
 
 func PrepareComputations(intersection Intersection, ray ray.Ray) Computations {
 	time := intersection.GetTime()
@@ -26,11 +28,19 @@ func PrepareComputations(intersection Intersection, ray ray.Ray) Computations {
 	eyev := vector.Negate(ray.Direction())
 	normalv := hitObj.NormalAt(point)
 
+	inside := true
+	if vector.Dot(eyev, normalv) < 0.0 {
+		normalv = vector.Negate(normalv)
+	} else {
+		inside = false
+	}
+
 	return Computations{
 		t:       time,
 		hitObj:  hitObj,
 		point:   point,
 		eyev:    eyev,
 		normalv: normalv,
+		inside:  inside,
 	}
 }
--- object/world_test.go
+++ object/world_test.go
@@ -62,3 +62,39 @@ func TestPrepareComputations(t *testing.T) {
 		t.Errorf("GetNormalV is %x not %x", comps.GetNormalV(), vecToTest)
 	}
 }
+
+func TestInside(t *testing.T) {
+	r := ray.CreateRay(vector.NewPoint(0.0, 0.0, -5.0), vector.NewVector(0.0, 0.0, 1.0))
+	shape := object.CreateSphere("s1")
+	i := object.CreateIntersection(&shape, 4)
+
+	comps := object.PrepareComputations(i, r)
+
+	if comps.IsInside() != false {
+		t.Errorf("TestInside: First intersection set to inside = true in PrepareComputations")
+	}
+}
+
+func TestInsideTwo(t *testing.T) {
+	r := ray.CreateRay(vector.NewPoint(0.0, 0.0, 0.0), vector.NewVector(0.0, 0.0, 1.0))
+	shape := object.CreateSphere("s1")
+	i := object.CreateIntersection(&shape, 1)
+
+	comps := object.PrepareComputations(i, r)
+
+	if vector.Equals(vector.NewPoint(0.0, 0.0, 1.0), comps.GetPoint()) == false {
+		t.Errorf("TestInsideTwo: comps.GetPoint() does not equal 0.0, 0.0, 1.0")
+	}
+
+	if vector.Equals(vector.NewVector(0.0, 0.0, -1.0), comps.GetEyeV()) == false {
+		t.Errorf("TestInsideTwo: EyeV does not equal 0.0, 0.0, -1.0")
+	}
+
+	if vector.Equals(vector.NewVector(0.0, 0.0, -1.0), comps.GetNormalV()) == false {
+		t.Errorf("TestInsideTwo: NormalV does not equal 0.0, 0.0, -1.0")
+	}
+
+	if comps.IsInside() != true {
+		t.Errorf("TestInsideTwo: Inside set to false, should be true")
+	}
+}
